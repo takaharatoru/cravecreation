9bf692bb9681fc451082299a1c98112c658e5ee7
Toru Takahara <56223595+takaharatoru@users.noreply.github.com>
2020-12-13 00:19:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,10 @@
-$('.slider-for').slick({
-    responsive:[
-        {
-            breakpoint: 1024,
-            settings:{
-                slidesToShow:1,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings:{
-                slidesToShow:1,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings:{
-                slidesToShow:1,
-            }
-        },
-    ]
-});
-$('.slider-nav').slick({
-    responsive:[
-        {
-            breakpoint: 1024,
-            settings:{
-                slidesToShow:1,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings:{
-                slidesToShow:1,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings:{
-                slidesToShow:1,
-            }
-        },
-    ]
-});
-
- $('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-  
-  
-});    
+$(document).ready(function(){
+    $('.slider-for').slick({
+      asNavFor: '.slider-nav',
+      arrows: false
+    });
+    $('.slider-nav').slick({
+      asNavFor: '.slider-for',
+      slide: 'li'
+    });
+  });
