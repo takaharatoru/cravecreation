2174c6e04124bab8ce8b2ab1dd7d8c0a8aceaeef
Toru Takahara <56223595+takaharatoru@users.noreply.github.com>
2020-12-12 23:28:48 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
-// $('.slider-for').slick({
-//     responsive:[
-//         {
-//             breakpoint: 1024,
-//             settings:{
-//                 slidesToShow:1,
-//             }
-//         },
-//         {
-//             breakpoint: 768,
-//             settings:{
-//                 slidesToShow:1,
-//             }
-//         },
-//         {
-//             breakpoint: 480,
-//             settings:{
-//                 slidesToShow:1,
-//             }
-//         },
-//     ]
-// });
-// $('.slider-nav').slick({
-//     responsive:[
-//         {
-//             breakpoint: 1024,
-//             settings:{
-//                 slidesToShow:1,
-//             }
-//         },
-//         {
-//             breakpoint: 768,
-//             settings:{
-//                 slidesToShow:1,
-//             }
-//         },
-//         {
-//             breakpoint: 480,
-//             settings:{
-//                 slidesToShow:1,
-//             }
-//         },
-//     ]
-// });
+$('.slider-for').slick({
+    responsive:[
+        {
+            breakpoint: 1024,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+    ]
+});
+$('.slider-nav').slick({
+    responsive:[
+        {
+            breakpoint: 1024,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+    ]
+});
 
  $('.slider-for').slick({
   slidesToShow: 1,
